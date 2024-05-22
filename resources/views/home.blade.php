d8af89ce7ce41158156ed88fbf8d40441e8f6a1e
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-22 17:18:47 +0200

    feat: add home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    ciao
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex flex-wrap gap-3 p-5">
+                @foreach ($trains as $train)
+                    <div class="card" style="width: calc(100% / 5 - 1rem);">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <h5 class="card-title">{{ $train->company }}</h5>
+                            <p class="card-text">{{$train->train_number}}</p>
+                            <div class="d-flex justify-content-between" >
+                                <small>{{ $train->origin}}</small>
+                                <small>{{ $train->destination}}</small>
+                            </div>
+                            <div class="d-flex justify-content-between" >
+                                <small>{{$train->departure_time}}</small>
+                                <small>{{$train->arrival_time}}</small>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
