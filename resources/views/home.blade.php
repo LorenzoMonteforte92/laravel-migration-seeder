d333a138bbfa42df37bc9210169588189cd45f9f
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-23 10:40:06 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,10 @@
                 @foreach ($trains as $train)
                     <div class="card" style="width: calc(100% / 5 - 1rem);">
                         <div class="card-body d-flex flex-column justify-content-between">
-                            <h5 class="card-title">{{ $train->company }}</h5>
-                            <p class="card-text">{{$train->train_number}}</p>
+                            <div class="d-flex flex-column justify-content-between" >
+                                <h5 class="card-title">{{ $train->company }}</h5>
+                                <p class="card-text">{{$train->train_number}}</p>
+                            </div>
                             <div class="d-flex justify-content-between" >
                                 <small>{{ $train->origin}}</small>
                                 <small>{{ $train->destination}}</small>
